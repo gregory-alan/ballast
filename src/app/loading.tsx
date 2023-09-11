4c5b3771797ea0c93f587d2b005a47e8c868712e
Grégory Alan <contact+ga@grebett.com>
2023-09-11 11:17:56 +0200

    wip: Home

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,16 +1,5 @@
 export default function Loading() {
   return (
-    <div
-      style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100vw',
-        height: '10vh',
-        background: 'red',
-      }}
-    >
-      <h1>HELLO</h1>
-    </div>
+    <></>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,60 @@
-import Link from 'next/link';
+'use client';
 
-export default function Home() {
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Cover from 'ballast/app/components/Cover';
+import ChapterSquare from 'ballast/app/components/ChapterSquare';
+import Splash from 'ballast/app/components/Splash';
+import { Sounds } from 'ballast/types/AudioService';
+import Link from 'next/link';
+import ButtonsBar from 'ballast/app/components/ButtonsBar';
+
+type ChapterInfo = { title: string; number: number; slug: string };
+
+export default function Book({
+  params,
+}: {
+  params: { book: string; chapter: string };
+}) {
+  const book = params.book;
+  const [chapters, setChapters] = useState<ChapterInfo[]>([]);
+  const [buttonsBarShown, toggleButtonsBar] = useState<boolean>(true);
+
+  // DATA IMPORT
+  useEffect(() => {
+    // // SOUNDS
+    // import(`ballast/data/books/${book}/infos.json`)
+    //   .then(({ default: infos }: { default: { chapters: ChapterInfo[] } }) => {
+    //     setChapters(infos.chapters);
+    //   })
+    //   .catch(() => {
+    //     // router.push('/404');
+    //   });
+  }, [book]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-slate-100">
-      <h1>BALLAST</h1>
-      <ul>
-        <li><Link href={"/book1/chapter1"}>HISTOIRE EXEMPLE 001</Link></li>
-        <li><Link href={"/book1/chapter2"}>HISTOIRE EXEMPLE 002</Link></li>
-        <li><Link href={"/book2/chapter1"}>HISTOIRE EXEMPLE 001</Link></li>
-        <li><Link href={"/book2/chapter1"}>HISTOIRE EXEMPLE 002</Link></li>
-      </ul>
+    <main className="flex max-w-md relative flex-col">
+      <ButtonsBar
+        top={4}
+        right={4}
+        width={10}
+        book={book}
+        audioMuted={false}
+        muteAudioHandler={() => {}}
+        isOpen={buttonsBarShown}
+        onExit={() => {}}
+        clickHandler={() => toggleButtonsBar(!buttonsBarShown)}
+        hide={{ sound: true, book: true }}
+      />
+
+      <Image
+        className="relative"
+        src={`/images/HOME.webp`}
+        alt="main"
+        width={10000}
+        height={1}
+        priority
+      />
     </main>
-  )
+  );
 }
